afcacbcc96b2d6b84408eec7aaa1c4a6e3c8db90
PauFugit <paulinefugit@gmail.com>
2022-09-09 17:34:14 +0000

    update

--- src/models.py
+++ src/models.py
@@ -8,26 +8,45 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(100), nullable=False, unique=True)
+    email = Column(String(100), nullable=False, unique=True)
+    password = Column(Integer, nullable=False)
+    name = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    likes = Column(Integer)
+    
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment = Column(String(200))
+    author_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
 
-    def to_dict(self):
-        return {}
+class Follower(Base):
+    __tablename__ = 'followers'
+    followed_id = Column(Integer, ForeignKey('users.id'), primary_key=True)
+    following_id = Column(Integer, ForeignKey('users.id'))
+    
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    m_file = Column(String(100))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+
+
+
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
